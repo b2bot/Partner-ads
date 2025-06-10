373f41772436826702ecef5cec72bf0449d21b92
b2bot <vagner@leadclinic.com.br>
2025-06-10 05:13:25 -0300

    Create objetivos-metricas.tsx

---
+++ src/pages/objetivos-metricas.tsx
@@ -0,0 +1,8 @@
+export default function ObjetivosMetricasPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Objetivos de Métricas</h1>
+      <p>Aqui vai a interface de configuração de objetivos por métrica.</p>
+    </div>
+  );
+}
