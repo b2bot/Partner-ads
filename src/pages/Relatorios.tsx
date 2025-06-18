018dcea5ab3f26c1a4060fdae3cfa7a0ea013484
b2bot <vagner@leadclinic.com.br>
2025-06-18 15:06:01 -0300

    Update Relatorios.tsx

--- src/pages/Relatorios.tsx
+++ src/pages/Relatorios.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import RelatoriosTab from '@/components/RelatoriosTab';
+import { RelatoriosTab } from '@/components/RelatoriosTab';
 
 export default function Relatorios() {
   return <RelatoriosTab />;
