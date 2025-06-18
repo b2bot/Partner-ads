6fe3b92a6fdb199d3ff93c37c0d059cc22fc9c75
b2bot <vagner@leadclinic.com.br>
2025-06-18 13:49:09 -0300

    Update main.tsx
    import { createRoot } from 'react-dom/client';
    import App from './App.tsx';
    import './index.css';
    
    import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
    
    // 1. Cria o client
    const queryClient = new QueryClient();
    
    // 2. Renderiza a App dentro do Provider
    createRoot(document.getElementById("root")!).render(
      <QueryClientProvider client={queryClient}>
        <App />
      </QueryClientProvider>
    );

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// 1. Cria o client
+const queryClient = new QueryClient();
+
+// 2. Renderiza a App dentro do Provider
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
