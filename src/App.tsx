35f9ad3c3c6521d02523e1feb248de2072a9862e
b2bot <vagner@leadclinic.com.br>
2025-06-20 08:09:14 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Toaster } from '@/components/ui/sonner';
-import { AuthWrapper } from '@/components/AuthWrapper';
-import { Dashboard } from '@/components/Dashboard';
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthWrapper } from "@/components/AuthWrapper";
 import { ResetPasswordForm } from '@/components/ResetPasswordForm';
-import Index from '@/pages/Index';
-import './App.css';
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+import { FiltersProvider } from "@/hooks/dashboard_hooks/useFilters";
+import { SettingsProvider } from "@/hooks/dashboard_hooks/useSettings";
 
 const queryClient = new QueryClient();
 
@@ -22,7 +24,7 @@ function App() {
               <AuthWrapper>
                 <Routes>
                   <Route path="/" element={<Index />} />
-                  <Route path="/dashboard" element={<Dashboard />} />
+				  <Route path="*" element={<NotFound />} />
                 </Routes>
               </AuthWrapper>
             } 
@@ -33,5 +35,7 @@ function App() {
     </QueryClientProvider>
   );
 }
+
+
 
 export default App;
